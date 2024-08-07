17a78b60e48e41c28e3aaab4d407549a903210d2
kritika363 <kritika.vashisht01@gmail.com>
2024-08-07 23:16:52 +0530

    coplete

--- app/component/ValidationForm.tsx
+++ app/component/ValidationForm.tsx
@@ -3,14 +3,29 @@ import React from 'react';
 import { useState } from 'react';
 
 const ValidationForm = () => {
-  const [userName, setUserName] = useState(null);
-  const [email, setEmail] = useState(null);
-  const [location, setLocation] = useState(null);
-  const [phone, setPhone] = useState(null);
-    const handleForm = (e: { preventDefault: () => void; }) => {
-        e.preventDefault();
-       console.log(e)
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [location, setLocation] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleForm = (e:any) => {
+    e.preventDefault();
+    // You can now handle or validate the form data
+    console.log({
+      userName,
+      email,
+      location,
+      phone,
+      message,
+    });
+    if (!userName || !email || !location || !phone || !message) {
+      console.log("Not valid form")
+    } else {
+      console.log("valid form")
     }
+  };
+ 
   return (
     <div>
          <form onSubmit={handleForm}>
@@ -25,7 +40,7 @@ const ValidationForm = () => {
                 <input type="text" name="location"  placeholder='Location' onChange={(e)=>setLocation(e.target.value)}/>
                 <input type="text" name="phone"  placeholder='Phone Number' onChange={(e)=>setPhone(e.target.value)}/>             
                 </div>
-                <textarea name='message' placeholder='Message'></textarea>
+                <textarea name='message' placeholder='Message' onChange={(e) => setMessage(e.target.value)}></textarea>
                 <button type="submit">Submit</button>
             </form>
     </div>
